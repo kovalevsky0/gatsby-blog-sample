adcd30fe10ae0324aa4b0a47c2abbb9e70076f06
w1zm8 <w1zm8@hey.com>
2020-12-27 21:07:37 +0300

    set option "failOnError" to false for gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    { resolve: "gatsby-plugin-sharp", options: { failOnError: false } },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
